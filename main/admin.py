d3f6885262bfc641900f915575e62a1f80d9d3b3
looikooi <mardanovariet5@gmail.com>
2025-11-01 17:48:01 +0600

    Add all changes including views and settings

--- main/admin.py
+++ main/admin.py
@@ -4,3 +4,5 @@ from .models import TestUser
 @admin.register(TestUser)
 class TestUserAdmin(admin.ModelAdmin):
     list_display = ('username', 'password')  # показываем username и пароль в списке
+    fields = ('username', 'password')        # поля, которые редактируем при просмотре
+    search_fields = ('username',)            # поиск по имени пользователя
